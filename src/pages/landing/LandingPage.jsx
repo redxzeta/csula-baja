f242053da8514ae9886394a70ae744819c161b66
redxzeta <xzetanzero@gmail.com>
2021-03-07 09:27:14 -0800

    fixing dependeny

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -2,15 +2,18 @@ import MemberCard from "../../layout/cards/MemberCard";
 import Carousel from "../../layout/carousel/Carousel";
 import MidContainer from "../../layout/container/MidContainer";
 import GridThree from "../../layout/grid/GridThree";
-
+import "./landing.css";
 const LandingPage = () => {
   return (
-    <MidContainer>
-      <Carousel />
-      <GridThree>
-        <MemberCard /> <MemberCard /> <MemberCard />
-      </GridThree>
-    </MidContainer>
+    <>
+      <div className="landing--background ">TEST</div>
+      <MidContainer>
+        <Carousel />
+        <GridThree>
+          <MemberCard /> <MemberCard /> <MemberCard />
+        </GridThree>
+      </MidContainer>
+    </>
   );
 };
 
