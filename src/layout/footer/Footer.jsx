f2f7a1100cb1c22fe88345b83c2e1dbe090f061d
redxzeta <xzetanzero@gmail.com>
2021-04-09 15:08:30 -0700

    footer fix

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -8,24 +8,24 @@ const socialLinks = [
   "https://www.youtube.com/user/CSULAbajaSAE",
 ];
 const Footer = () => (
-  <footer className="w-full md:h-80 h-100% bg-black p-10 md:p-12 text-indigo-50 flex flex-col md:flex-row md:justify-between justify-evenly">
-    <div className="flex flex-1 w-full md:flex-row flex-col p-10 md:p-0">
+  <footer className="w-full lg:h-80 h-100% bg-black p-10 lg:p-12 text-indigo-50 flex flex-col lg:flex-row lg:justify-between justify-evenly">
+    <div className="flex flex-1 w-full lg:flex-row flex-col p-10 lg:p-0">
       <img
         src={QR}
         alt="qr code"
-        className="md:h-1/2 w-1/2 md:w-auto h-auto mx-auto my-auto "
+        className="lg:h-1/2 w-1/2 lg:w-auto h-auto mx-auto my-auto "
       />
-      <div className="flex-1 w-full flex flex-col h-full p-5 md:justify-center md:items-start items-left ">
+      <div className="flex-1 w-full flex flex-col h-full p-5 lg:justify-center lg:items-start items-left ">
         <p className="text-left">Want to join the team?</p>
         <p className="text-left">Scan the QR code to join!</p>
       </div>
     </div>
-    <div className="flex-1 w-full flex md:justify-center items-center h-full p-2 justify-evenly ">
+    <div className="flex-1 w-full flex lg:justify-center items-center h-full p-2 justify-evenly ">
       {socialLinks.map((url) => (
-        <SocialIcon key={url} fgColor="white" url={url} className="md:mx-3" />
+        <SocialIcon key={url} fgColor="white" url={url} className=" lg:mx-3" />
       ))}
     </div>
-    <div className="flex-1 md:text-right text-left px-5 md:px-0">
+    <div className="flex-1 lg:text-right text-left px-5 lg:px-0">
       <p className="text-2xl">Cal State LA Baja SAE</p>
       <p>5151 State University Drive</p>
       <p>Los Angeles, CA 90032</p>
